9ad4f3f37293d1249e051d9dd87a0e3fb39aaae5
syamkumar0418 <syamkumar0418@gmail.com>
2024-12-17 15:46:52 +0530

    add to cart icon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */ 
-module.exports = { 
-    output: 'export',
-    trailingSlash: true, // Optional, useful for making URLs cleaner (adds trailing slash)
+module.exports = {  
   };
   
